d5babc032923193fb0518cd74881d8d6c4501b06
TE-TakeshiBYoshida <takeshi.yoshida@sharing-innovations.co.jp>
2019-04-17 18:56:30 +0900

    Initial commit

---
+++ app/src/main/java/com/stah/lifecybleleview/MainActivity.kt
@@ -0,0 +1,20 @@
+// Copyright (c) 2019. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+// Morbi non lorem porttitor neque feugiat blandit. Ut vitae ipsum eget quam lacinia accumsan.
+// Etiam sed turpis ac ipsum condimentum fringilla. Maecenas magna.
+// Proin dapibus sapien vel ante. Aliquam erat volutpat. Pellentesque sagittis ligula eget metus.
+// Vestibulum commodo. Ut rhoncus gravida arcu.
+
+package com.stah.lifecybleleview
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        println("onCreate : ${lifecycle.currentState.name}")
+    }
+}
